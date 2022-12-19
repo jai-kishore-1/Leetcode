3389826e1ec65facf758bfdfcae0979145710099
jai-kishore-1 <jaikishore0106@gmail.com>
2022-12-19 21:16:41 +0530

    Kishore

--- 1_Longest_common_prefix.java
+++ 1_Longest_common_prefix.java
@@ -1,7 +1,6 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
         String res = "";
-
         for(int i = 0; i < strs[0].length(); i++) {
             char c = strs[0].charAt(i);
             for (int j = 1; j < strs.length; j++) {
